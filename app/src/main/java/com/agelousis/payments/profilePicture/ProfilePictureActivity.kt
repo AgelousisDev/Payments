2fd9f34f7b8147070b3efaf6e1c60c3a430aa952
Vagelis Agelousis <vagelis_agelousis@outlook.com>
2020-11-26 22:16:30 +0200

    Payments
    Add swipe down to dismiss the ProfilePictureActivity

--- app/src/main/java/com/agelousis/payments/profilePicture/ProfilePictureActivity.kt
+++ app/src/main/java/com/agelousis/payments/profilePicture/ProfilePictureActivity.kt
@@ -4,18 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.agelousis.payments.databinding.ActivityProfilePictureBinding
 import com.agelousis.payments.main.MainActivity
+import com.thefuntasty.hauler.setOnDragDismissedListener
 
 class ProfilePictureActivity : AppCompatActivity() {
 
+    private var binding: ActivityProfilePictureBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityProfilePictureBinding.inflate(layoutInflater)
+        binding?.userModel = intent?.extras?.getParcelable(MainActivity.USER_MODEL_EXTRA)
         setContentView(
-            ActivityProfilePictureBinding.inflate(
-                layoutInflater
-            ).also {
-                it.userModel = intent?.extras?.getParcelable(MainActivity.USER_MODEL_EXTRA)
-            }.root
+            binding?.root
         )
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding?.haulerView?.setOnDragDismissedListener {
+            finish()
+        }
     }
 
 }
