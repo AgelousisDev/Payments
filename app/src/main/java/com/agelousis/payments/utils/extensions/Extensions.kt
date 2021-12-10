73cea6aab33185cf965a996027cfeb99752f4fe1
agelousis <vagelis_agelousis@outlook.com>
2021-12-10 20:18:55 +0200

    Payments
    Fix plusMonths calendar extension

--- app/src/main/java/com/agelousis/payments/utils/extensions/Extensions.kt
+++ app/src/main/java/com/agelousis/payments/utils/extensions/Extensions.kt
@@ -567,8 +567,13 @@ val String.toRawMobileNumber
 fun Date.toCalendar(plusMonths: Int? = null): Calendar {
     val calendar = Calendar.getInstance()
     calendar.time = this
-    calendar.set(Calendar.YEAR, if (calendar.get(Calendar.MONTH) == 11 && plusMonths != null) calendar.get(Calendar.YEAR) + 1 else calendar.get(Calendar.YEAR))
-    calendar.set(Calendar.MONTH, if (calendar.get(Calendar.MONTH) == 11 && plusMonths != null) 0 else calendar.get(Calendar.MONTH) + (plusMonths ?: 0))
+    if (plusMonths != null)
+        calendar.add(
+            Calendar.MONTH,
+            plusMonths
+        )
+    //calendar.set(Calendar.YEAR, if (calendar.get(Calendar.MONTH) == 11 && plusMonths != null) calendar.get(Calendar.YEAR) + 1 else calendar.get(Calendar.YEAR))
+    //calendar.set(Calendar.MONTH, if (calendar.get(Calendar.MONTH) == 11 && plusMonths != null) 0 + plusMonths else calendar.get(Calendar.MONTH) + (plusMonths ?: 0))
     return calendar
 }
 
