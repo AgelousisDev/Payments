c97534254eabc549803676669187c23803ad7a6e
agelousis <vagelis_agelousis@outlook.com>
2022-03-18 09:12:27 +0200

    Payments
    Migrate ChartFragment to Compose

--- app/src/main/java/com/agelousis/payments/main/ui/dashboard/ChartFragment.kt
+++ app/src/main/java/com/agelousis/payments/main/ui/dashboard/ChartFragment.kt
@@ -1,29 +1,18 @@
 package com.agelousis.payments.main.ui.dashboard
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
-import com.agelousis.payments.R
-import com.agelousis.payments.databinding.ChartFragmentLayoutBinding
-import com.agelousis.payments.main.ui.dashboard.adapters.GroupDataAdapter
 import com.agelousis.payments.main.ui.dashboard.enumerations.ChartType
-import com.agelousis.payments.main.ui.dashboard.listeners.PaymentLineChartGestureListener
-import com.agelousis.payments.main.ui.payments.models.GroupModel
-import com.agelousis.payments.utils.constants.Constants
-import com.agelousis.payments.utils.extensions.euroFormattedString
-import com.agelousis.payments.utils.extensions.toast
-import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import java.text.SimpleDateFormat
-import java.util.*
+import com.agelousis.payments.main.ui.dashboard.ui.ChartLayout
+import com.agelousis.payments.ui.Typography
+import com.agelousis.payments.ui.appColors
 
 class ChartFragment: Fragment() {
 
@@ -43,7 +32,6 @@ class ChartFragment: Fragment() {
 
     }
 
-    private lateinit var binding: ChartFragmentLayoutBinding
     private val chartType by lazy {
         arguments?.getSerializable(
             CHART_TYPE_EXTRA
@@ -56,222 +44,33 @@ class ChartFragment: Fragment() {
         (parentFragment as? HistoryFragment)?.paymentAmountModelList
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = ChartFragmentLayoutBinding.inflate(
-            inflater,
-            container,
-            false
-        ).also {
-            it.chartType = chartType
-        }
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupUI()
-    }
-
-    private fun setupUI() {
-        when(chartType) {
-            ChartType.LINE_CHART -> {
-                val payments = arrayListOf(
-                    *clientModelList?.asSequence()?.mapNotNull {
-                        it.payments
-                    }?.flatten()?.toList()?.toTypedArray() ?: arrayOf(),
-                    *paymentAmountModelList?.toTypedArray() ?: arrayOf()
-                )
-                configureLineChart(
-                    entries = payments.sortedBy {
-                        it.paymentMonthDate
-                    }.groupBy {
-                        it.paymentMonthDate
-                    }.map { map ->
-                        Entry(
-                            map.key?.time?.toFloat() ?: 0f,
-                            map.value.mapNotNull { it.paymentAmount }.sum().toFloat()
-                        )
-                    }.toList()
-                )
-            }
-            ChartType.PIE_CHART -> {
-                configureGroupDataRecyclerView()
-                configurePieChart(
-                    colors = clientModelList?.sortedBy {
-                        it.groupName
-                    }?.groupBy {
-                        it.groupName
-                    }?.map {
-                        it.value.firstOrNull()?.groupColor ?: 0
-                    } ?: return,
-                    entries = clientModelList?.sortedBy {
-                        it.groupName
-                    }?.groupBy {
-                        it.groupName
-                    }?.map { map ->
-                        val groupPaymentAmountPercentage = (clientModelList?.asSequence()?.filter {
-                            it.groupName == map.key
-                        }?.mapNotNull { clientModel ->
-                            clientModel.payments
-                        }?.flatten()?.mapNotNull { paymentAmountModel ->
-                            paymentAmountModel.paymentAmount
-                        }?.sum()?.toFloat() ?: 0f) / (clientModelList?.mapNotNull { paymentAmountModel ->
-                            paymentAmountModel.payments
-                        }?.flatten()?.mapNotNull { paymentAmountModel ->
-                            paymentAmountModel.paymentAmount
-                        }?.sum()?.toFloat() ?: 0f) * 100
-                        val groupName = when {
-                            groupPaymentAmountPercentage <= 4f ->
-                                null
-                            else ->
-                                if ((map.value.firstOrNull()?.groupName?.length ?: 0) > 12)
-                                    "${map.value.firstOrNull()?.groupName?.take(n = 9)}..."
-                                else
-                                    map.value.firstOrNull()?.groupName
-                        }
-                        PieEntry(
-                            groupPaymentAmountPercentage,
-                            groupName
-                        )
-                    } ?: return
-                )
-            }
-            else -> {}
-        }
-    }
-
-    private fun configureLineChart(entries: List<Entry>) {
-        val desc = Description()
-        desc.text = ""
-        binding.lineChart.description = desc
-        binding.lineChart.legend?.isEnabled = false
-        binding.lineChart.xAxis?.textColor = ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground)
-        binding.lineChart.xAxis?.position = XAxis.XAxisPosition.BOTTOM
-        binding.lineChart.xAxis?.valueFormatter = object: ValueFormatter() {
-            override fun getFormattedValue(value: Float) =
-                SimpleDateFormat(Constants.GRAPH_DATE_FORMAT, Locale.US).format(value.toLong())
-        }
-        //lineChart.xAxis.setAvoidFirstLastClipping(true)
-        binding.lineChart.xAxis?.isGranularityEnabled = true
-        binding.lineChart.xAxis?.setDrawLimitLinesBehindData(true)
-        binding.lineChart.axisLeft?.valueFormatter = object: ValueFormatter() {
-            override fun getFormattedValue(value: Float) =
-                if (value == 0f)
-                    "0"
-                else
-                    value.toDouble().euroFormattedString
-        }
-        binding.lineChart.axisLeft?.textColor = ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground)
-        binding.lineChart.axisRight?.isEnabled = false
-        binding.lineChart.onChartGestureListener =
-            object: PaymentLineChartGestureListener(chart = binding.lineChart) {
-                override fun onAmountSelected(amount: String) {
-                    context?.toast(
-                        message = amount
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(
+            context = context ?: return null
+        ).apply {
+            setContent {
+                MaterialTheme(
+                    typography = Typography,
+                    colors = appColors()
+                ) {
+                    ChartLayout(
+                        chartType = chartType ?: return@MaterialTheme,
+                        clientModelList = clientModelList ?: return@MaterialTheme,
+                        paymentAmountModelList = paymentAmountModelList ?: return@MaterialTheme
                     )
                 }
             }
-        //lineChart.xAxis.setLabelCount(entries.size, true)
-        binding.lineChart.xAxis?.granularity = 1f
-        //lineChart.setMaxVisibleValueCount(4)
-        setLineChartData(
-            entries = entries
-        )
+        }
     }
 
-    private fun setLineChartData(entries: List<Entry>) {
-        val dataSets = arrayListOf<ILineDataSet>()
-        dataSets.add(
-            LineDataSet(
-                entries,
-                resources.getString(R.string.key_amount_label)
-            ).also {
-                it.setDrawFilled(true)
-                it.fillDrawable = ContextCompat.getDrawable(context ?: return, R.drawable.graph_draw_gradient_background)
-                it.setDrawCircles(true)
-                it.circleRadius = 4.0f
-                it.setDrawValues(false)
-                it.lineWidth = 3.0f
-                it.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
-                it.color = ContextCompat.getColor(context ?: return, R.color.colorAccent)
-                it.setCircleColor(ContextCompat.getColor(context ?: return, R.color.colorAccent))
-                it.highLightColor = ContextCompat.getColor(context ?: return, R.color.green)
-            }
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun ChartFragmentPreview() {
+        ChartLayout(
+            chartType = ChartType.PIE_CHART,
+            clientModelList = clientModelList ?: return,
+            paymentAmountModelList = paymentAmountModelList ?: return
         )
-        binding.lineChart.data = LineData(dataSets)
-        binding.lineChart.invalidate()
-    }
-
-    private fun configureGroupDataRecyclerView() {
-        binding.groupDataRecyclerView.adapter = GroupDataAdapter(
-            groupModelList = clientModelList?.groupBy { it.groupName }?.map { map ->
-                GroupModel(
-                    color = map.value.first().groupColor,
-                    groupName = map.key
-                )
-            } ?: listOf()
-        )
-    }
-
-    private fun configurePieChart(colors: List<Int>, entries: List<PieEntry>) {
-        binding.pieChart.isDrawHoleEnabled = true
-        binding.pieChart.setUsePercentValues(true)
-        binding.pieChart.setEntryLabelTextSize(12f)
-        binding.pieChart.setEntryLabelColor(ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground))
-        binding.pieChart.centerText = resources.getString(R.string.key_incoming_payments_label)
-        binding.pieChart.setCenterTextSize(24f)
-        binding.pieChart.description.isEnabled = false
-        binding.pieChart.setBackgroundColor(Color.TRANSPARENT)
-        binding.pieChart.setHoleColor(Color.TRANSPARENT)
-        binding.pieChart.setCenterTextColor(ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground))
-        binding.pieChart.isRotationEnabled = false
-        binding.pieChart.holeRadius = 55f
-        binding.pieChart.renderer.paintRender.setShadowLayer(
-            4f,
-            0f,
-            0f,
-            ContextCompat.getColor(
-                context ?: return,
-                R.color.dayNightTextOnBackground
-            )
-        )
-
-        /*val legend = binding.pieChart.legend
-        legend.textColor = ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground)
-        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
-        legend.orientation = Legend.LegendOrientation.HORIZONTAL
-        legend.setDrawInside(false)
-        legend.form = Legend.LegendForm.CIRCLE
-        legend.isEnabled = true
-        binding.pieChart.legend.isWordWrapEnabled = true*/
-        binding.pieChart.legend.isEnabled = false
-        loadPieChartData(
-            colors = colors,
-            entries = entries
-        )
-    }
-
-    private fun loadPieChartData(colors: List<Int>, entries: List<PieEntry>) {
-        val dataSet = PieDataSet(entries, null)
-        dataSet.colors = colors
-
-        val data = PieData(dataSet)
-        data.setValueFormatter(
-            object: ValueFormatter() {
-                override fun getFormattedValue(value: Float): String {
-                    return if (value > 4f) String.format("%.1f%%", value) else ""
-                }
-            }
-        )
-        data.setDrawValues(true)
-        data.setValueTextSize(12f)
-        data.setValueTextColor(ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground))
-
-        binding.pieChart.data = data
-        binding.pieChart.invalidate()
-
-        binding.pieChart.animateY(1400, Easing.EaseOutCirc)
     }
 
 }
---
+++ app/src/main/java/com/agelousis/payments/main/ui/dashboard/ui/ChartUI.kt
@@ -0,0 +1,258 @@
+package com.agelousis.payments.main.ui.dashboard.ui
+
+import android.content.res.Configuration
+import android.graphics.Color
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.core.content.ContextCompat
+import com.agelousis.payments.R
+import com.agelousis.payments.main.ui.dashboard.enumerations.ChartType
+import com.agelousis.payments.main.ui.dashboard.listeners.PaymentLineChartGestureListener
+import com.agelousis.payments.main.ui.payments.models.ClientModel
+import com.agelousis.payments.main.ui.payments.models.PaymentAmountModel
+import com.agelousis.payments.utils.constants.Constants
+import com.agelousis.payments.utils.extensions.euroFormattedString
+import com.agelousis.payments.utils.extensions.toast
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Composable
+fun ChartLayout(
+    chartType: ChartType,
+    clientModelList: List<ClientModel>,
+    paymentAmountModelList: List<PaymentAmountModel>
+) {
+    val orientation = LocalConfiguration.current.orientation
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val (pieChartConstrainedReference, lineChartConstrainedReference) = createRefs()
+        when(chartType) {
+            ChartType.PIE_CHART ->
+                AndroidView(
+                    factory = { context ->
+                        PieChart(context).apply {
+                            configurePieChart(
+                                colors = clientModelList.sortedBy {
+                                    it.groupName
+                                }.groupBy {
+                                    it.groupName
+                                }.map {
+                                    it.value.firstOrNull()?.groupColor ?: 0
+                                },
+                                entries = clientModelList.sortedBy {
+                                    it.groupName
+                                }.groupBy {
+                                    it.groupName
+                                }.map { map ->
+                                    val groupPaymentAmountPercentage = clientModelList.asSequence().filter {
+                                        it.groupName == map.key
+                                    }.mapNotNull { clientModel ->
+                                        clientModel.payments
+                                    }.flatten().mapNotNull { paymentAmountModel ->
+                                        paymentAmountModel.paymentAmount
+                                    }.sum().toFloat() / clientModelList.mapNotNull { paymentAmountModel ->
+                                        paymentAmountModel.payments
+                                    }.flatten().mapNotNull { paymentAmountModel ->
+                                        paymentAmountModel.paymentAmount
+                                    }.sum().toFloat() * 100
+                                    val groupName = when {
+                                        groupPaymentAmountPercentage <= 4f ->
+                                            null
+                                        else ->
+                                            if ((map.value.firstOrNull()?.groupName?.length ?: 0) > 12)
+                                                "${map.value.firstOrNull()?.groupName?.take(n = 9)}..."
+                                            else
+                                                map.value.firstOrNull()?.groupName
+                                    }
+                                    PieEntry(
+                                        groupPaymentAmountPercentage,
+                                        groupName
+                                    )
+                                }
+                            )
+                        }
+                    },
+                    modifier = Modifier
+                        .constrainAs(pieChartConstrainedReference) {
+                            start.linkTo(parent.start, if (orientation == Configuration.ORIENTATION_LANDSCAPE) 0.dp else 32.dp)
+                            top.linkTo(parent.top, if (orientation == Configuration.ORIENTATION_LANDSCAPE) 0.dp else 32.dp)
+                            end.linkTo(parent.end, if (orientation == Configuration.ORIENTATION_LANDSCAPE) 0.dp else 32.dp)
+                            bottom.linkTo(parent.bottom, 90.dp)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.fillToConstraints
+                        }
+                )
+            ChartType.LINE_CHART ->
+                AndroidView(
+                    factory = { context ->
+                        LineChart(context).apply {
+                            val payments = arrayListOf(
+                                *clientModelList.asSequence().mapNotNull {
+                                    it.payments
+                                }.flatten().toList().toTypedArray(),
+                                *paymentAmountModelList.toTypedArray()
+                            )
+                            this configureLineChart payments.sortedBy {
+                                    it.paymentMonthDate
+                                }.groupBy {
+                                    it.paymentMonthDate
+                                }.map { map ->
+                                    Entry(
+                                        map.key?.time?.toFloat() ?: 0f,
+                                        map.value.mapNotNull { it.paymentAmount }.sum().toFloat()
+                                    )
+                                }.toList()
+                        }
+                    },
+                    modifier = Modifier
+                        .constrainAs(lineChartConstrainedReference) {
+                            start.linkTo(parent.start)
+                            top.linkTo(parent.top)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom, 90.dp)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.fillToConstraints
+                        }
+                )
+        }
+    }
+}
+
+private fun PieChart.configurePieChart(colors: List<Int>, entries: List<PieEntry>) {
+    isDrawHoleEnabled = true
+    setUsePercentValues(true)
+    setEntryLabelTextSize(12f)
+    setEntryLabelColor(ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground))
+    centerText = resources.getString(R.string.key_incoming_payments_label)
+    setCenterTextSize(24f)
+    description.isEnabled = false
+    setBackgroundColor(Color.TRANSPARENT)
+    setHoleColor(Color.TRANSPARENT)
+    setCenterTextColor(ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground))
+    isRotationEnabled = false
+    holeRadius = 55f
+    renderer.paintRender.setShadowLayer(
+        4f,
+        0f,
+        0f,
+        ContextCompat.getColor(
+            context ?: return,
+            R.color.dayNightTextOnBackground
+        )
+    )
+
+    /*val legend = binding.pieChart.legend
+    legend.textColor = ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground)
+    legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+    legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+    legend.orientation = Legend.LegendOrientation.HORIZONTAL
+    legend.setDrawInside(false)
+    legend.form = Legend.LegendForm.CIRCLE
+    legend.isEnabled = true
+    legend.isWordWrapEnabled = true*/
+    legend.isEnabled = false
+    loadPieChartData(
+        colors = colors,
+        entries = entries
+    )
+}
+
+private fun PieChart.loadPieChartData(colors: List<Int>, entries: List<PieEntry>) {
+    val dataSet = PieDataSet(entries, null)
+    dataSet.colors = colors
+
+    val data = PieData(dataSet)
+    data.setValueFormatter(
+        object: ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return if (value > 4f) String.format("%.1f%%", value) else ""
+            }
+        }
+    )
+    data.setDrawValues(true)
+    data.setValueTextSize(12f)
+    data.setValueTextColor(ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground))
+
+    this.data = data
+    invalidate()
+
+    animateY(1400, Easing.EaseOutCirc)
+}
+
+private infix fun LineChart.configureLineChart(entries: List<Entry>) {
+    val desc = Description()
+    desc.text = ""
+    description = desc
+    legend?.isEnabled = false
+    xAxis?.textColor = ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground)
+    xAxis?.position = XAxis.XAxisPosition.BOTTOM
+    xAxis?.valueFormatter = object: ValueFormatter() {
+        override fun getFormattedValue(value: Float) =
+            SimpleDateFormat(Constants.GRAPH_DATE_FORMAT, Locale.US).format(value.toLong())
+    }
+    //lineChart.xAxis.setAvoidFirstLastClipping(true)
+    xAxis?.isGranularityEnabled = true
+    xAxis?.setDrawLimitLinesBehindData(true)
+    axisLeft?.valueFormatter = object: ValueFormatter() {
+        override fun getFormattedValue(value: Float) =
+            if (value == 0f)
+                "0"
+            else
+                value.toDouble().euroFormattedString
+    }
+    axisLeft?.textColor = ContextCompat.getColor(context ?: return, R.color.dayNightTextOnBackground)
+    axisRight?.isEnabled = false
+    onChartGestureListener =
+        object: PaymentLineChartGestureListener(
+            chart = this
+        ) {
+            override fun onAmountSelected(amount: String) {
+                context?.toast(
+                    message = amount
+                )
+            }
+        }
+    //setLabelCount(entries.size, true)
+    xAxis?.granularity = 1f
+    //setMaxVisibleValueCount(4)
+    this setLineChartData entries
+}
+
+private infix fun LineChart.setLineChartData(entries: List<Entry>) {
+    val dataSets = arrayListOf<ILineDataSet>()
+    dataSets.add(
+        LineDataSet(
+            entries,
+            resources.getString(R.string.key_amount_label)
+        ).also {
+            it.setDrawFilled(true)
+            it.fillDrawable = ContextCompat.getDrawable(context ?: return, R.drawable.graph_draw_gradient_background)
+            it.setDrawCircles(true)
+            it.circleRadius = 4.0f
+            it.setDrawValues(false)
+            it.lineWidth = 3.0f
+            it.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+            it.color = ContextCompat.getColor(context ?: return, R.color.colorAccent)
+            it.setCircleColor(ContextCompat.getColor(context ?: return, R.color.colorAccent))
+            it.highLightColor = ContextCompat.getColor(context ?: return, R.color.green)
+        }
+    )
+    data = LineData(dataSets)
+    invalidate()
+}
